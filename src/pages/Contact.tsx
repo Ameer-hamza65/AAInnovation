c6af2d9ac19baa2789939f4a13948774d6212796
Ameer-hamza65 <22-se-38@students.uettaxila.edu.pk>
2026-04-05 13:21:53 +0500

    update38

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -82,8 +82,8 @@ const Contact = () => {
   return (
     <div className="relative min-h-screen bg-white">
       <div
-        className="fixed bottom-0 left-0 right-0 h-[260px] md:h-[340px] pointer-events-none opacity-70 z-0"
-        style={{ backgroundImage: `url(${goldWaveBottom})`, backgroundSize: '100% 100%', backgroundPosition: 'bottom center', backgroundRepeat: 'no-repeat' }}
+className="fixed bottom-0 left-0 right-0 h-[350px] md:h-[500px] pointer-events-none opacity-70 z-0"
+        style={{ backgroundImage: `url(${goldWaveBottom})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}
       />
       <div className="relative z-10">
       <Navbar />
